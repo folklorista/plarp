9bc494428f63e41e593e3683ff728be70656ae83
Stanislav Škubal <skubal@dudr.cz>
2020-06-30 11:00:34 +0200

    API: initial lb4 app

---
+++ api/src/__tests__/acceptance/test-helper.ts
@@ -0,0 +1,32 @@
+import {PlarpApplication} from '../..';
+import {
+  createRestAppClient,
+  givenHttpServerConfig,
+  Client,
+} from '@loopback/testlab';
+
+export async function setupApplication(): Promise<AppWithClient> {
+  const restConfig = givenHttpServerConfig({
+    // Customize the server configuration here.
+    // Empty values (undefined, '') will be ignored by the helper.
+    //
+    // host: process.env.HOST,
+    // port: +process.env.PORT,
+  });
+
+  const app = new PlarpApplication({
+    rest: restConfig,
+  });
+
+  await app.boot();
+  await app.start();
+
+  const client = createRestAppClient(app);
+
+  return {app, client};
+}
+
+export interface AppWithClient {
+  app: PlarpApplication;
+  client: Client;
+}
